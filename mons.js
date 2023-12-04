3f3a5a7099c8edbfeb1ed32600a8a8766c1ee6f7
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-12-04 14:39:59 -0500

    js stuff

--- mons.js
+++ mons.js
@@ -1,1 +1,17 @@
-//java here
+let monArray =[]
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+.then(res => res.json())
+.then(mons =>  {
+    monArray = mons.results;
+    //console.log(monArray);
+    const monList = document.querySelector('#dex-list');
+    monArray.forEach(mon => {
+        const monListElement = document.createElement('li');
+        const monName = monArray.name;
+        const monIndex = monArray.index;
+        monListElement.textContent = `${monIndex} - ${monName}`
+        monList.appendChild(monListElement);
+        //console.log(monIndex)
+        //console.log(mon);
+    })
+})
